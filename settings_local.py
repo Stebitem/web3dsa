613a5ed97cb1f8a32acf547c38f0f971a51fdc69
stebitem <stebitem@gmail.com>
2025-10-26 20:55:45 -0300

    WEB 3DSA

--- settings_local.py
+++ settings_local.py
@@ -4,7 +4,7 @@ BASE_DIR = Path(__file__).resolve().parent
 
 SECRET_KEY = 'django-insecure-reemplazar-esta-clave-en-produccion'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
