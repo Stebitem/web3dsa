9854c1d797adb950ec1ff8f5b51266aa6287c706
stebitem <stebitem@gmail.com>
2025-10-30 20:43:02 -0300

    1122

--- settings.py
+++ settings.py
@@ -3,8 +3,10 @@ import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Usar variable de entorno en producción
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-reemplazar-esta-clave-en-produccion')
-DEBUG = False
+
+DEBUG = False  # Siempre False en producción
 
 ALLOWED_HOSTS = ['web3dsa.onrender.com', 'localhost', '127.0.0.1']
 CSRF_TRUSTED_ORIGINS = ['https://web3dsa.onrender.com']
@@ -38,8 +40,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-AUTH_PASSWORD_VALIDATORS = []
 
 LANGUAGE_CODE = 'es-es'
 TIME_ZONE = 'UTC'
--- shopproject/wsgi.py
+++ shopproject/wsgi.py
@@ -1,6 +1,5 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shopproject.settings')
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web3dsa.settings')
 application = get_wsgi_application()
