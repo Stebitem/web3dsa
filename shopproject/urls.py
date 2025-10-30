562ac08eb4e428f425d53766b6a00535ff526556
stebitem <stebitem@gmail.com>
2025-10-30 20:49:59 -0300

    12

--- shopproject/urls.py
+++ shopproject/urls.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myshop.urls')),
+    # Redirigir favicon.ico al archivo estático
+    path('favicon.ico', RedirectView.as_view(url=settings.STATIC_URL + 'favicon.ico')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Para servir media en desarrollo
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
